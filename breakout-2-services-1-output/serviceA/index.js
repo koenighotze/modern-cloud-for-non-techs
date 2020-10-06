45b34819cd45b4e190e42be4c1d24d74bb43bafd
David <david.schmitz.privat@gmail.com>
2020-10-06 10:44:16 +0200

    use port from env

--- breakout-2-services-1-output/serviceA/index.js
+++ breakout-2-services-1-output/serviceA/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const axios = require('axios').default
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/hello/:name', async (req, res) => {
   // this should point to service B
