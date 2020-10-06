4a56bd7f4afa2aab7f982385f6db85c8ad042cc5
David <david.schmitz.privat@gmail.com>
2020-10-06 11:01:13 +0200

    Add cross service call

--- breakout-2-services-1-output/serviceA/index.js
+++ breakout-2-services-1-output/serviceA/index.js
@@ -4,10 +4,16 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/hello/:name', async (req, res) => {
-  // this should point to service B
-  const response = await axios.get('http://worldtimeapi.org/api/timezone/Europe/Berlin')
+  try {
+    const response = await axios.get('https://service-b-dot-dev1-onb-playground-18fd.appspot.com/now', {
+      timeout: 3000
+    })
 
-  res.send(`Hello ${req.params.name}. The date is ${response.data.datetime}`)
+    res.send(`Hello ${req.params.name}. The date is ${response.data.date}`)
+  }
+  catch (e) {
+    res.status(500).send({ error: `Cannot fetch current date time ${e.message}`})
+  }
 })
 
 app.listen(port, () => {
