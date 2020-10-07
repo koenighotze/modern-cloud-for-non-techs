ae5e96d443a77c81452986d66638eecb8a03c76c
David <david.schmitz.privat@gmail.com>
2020-10-07 15:29:22 +0200

    With traffic shift

--- breakout-2-services-1-output/serviceA/index.js
+++ breakout-2-services-1-output/serviceA/index.js
@@ -2,12 +2,18 @@ const express = require('express')
 const axios = require('axios').default
 const app = express()
 const port = process.env.PORT || 3000
+const serviceBUrl = process.env.SERVICE_B_URL
 
 app.get('/hello/:name', async (req, res) => {
-  // this should point to service B
-  const response = await axios.get('http://worldtimeapi.org/api/timezone/Europe/Berlin')
+  try {
+    const response = await axios.get(`${serviceBUrl}/now`, { timeout: 3000 })
 
-  res.send(`Hello ${req.params.name}. The date is ${response.data.datetime}`)
+    res.send(`Hello ${req.params.name}. The date is ${response.data.date}. Full response ${JSON.stringify(response.data)}`)
+  }
+  catch (e) {
+    res.status(500).send(e.message)
+  }
+
 })
 
 app.listen(port, () => {
